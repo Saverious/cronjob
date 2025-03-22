521110505365b1cb8542eabc3459f53a26ada2f8
saverious <saverious7@gmail.com>
2025-03-22 03:59:55 +0300

    chore: omit unused code

--- test/lib/time.js
+++ test/lib/time.js
@@ -1,17 +1,8 @@
-let now = new Date();
 let date1 = new Date();
 date1.setMinutes(date1.getMinutes() - 30);  // past date
 let date2 = new Date();
 date2.setMinutes(date2.getMinutes() + 30);  // future date
 
-// dates
-export const thisDate = {
-    year: now.getFullYear(),
-    month: now.getMonth() + 1,
-    day: now.getDate(),
-    hour: now.getHours(),
-    minute: now.getMinutes()
-};
 
 export const pastTime1 = {
     year: date1.getFullYear(),
